23d225c054d0055bf90c4db21e1c4ee0469cc5b6
t1ww <95153968+t1ww@users.noreply.github.com>
2024-08-29 21:56:53 +0700

    5.5

--- src/main/java/se331/lab/rest/dao/EventDaoImpl.java
+++ src/main/java/se331/lab/rest/dao/EventDaoImpl.java
@@ -120,22 +120,15 @@ public class EventDaoImpl implements EventDao {
     public List<Event> getEvents(Integer pageSize, Integer page) {
         pageSize = pageSize == null ? eventList.size() : pageSize;
         page = page == null ? 1 : page;
-        Integer firstIndex = (page - 1) * pageSize;
-        List<Event> output = new ArrayList<>();
-        for (int i = firstIndex; i < firstIndex + pageSize && i < eventList.size(); i++) {
-            output.add(eventList.get(i));
-        }
-        return output;
+        int firstIndex = (page - 1) * pageSize;
+        return eventList.subList(firstIndex, Math.min(firstIndex + pageSize, eventList.size()));
     }
+
     @Override
     public Event getEventById(Long id) {
-        Event output = null;
-        for (Event event : eventList) {
-            if (event.getId().equals(id)) {
-                output = event;
-                break;
-            }
-        }
-        return output;
+        return eventList.stream()
+                .filter(event -> event.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
