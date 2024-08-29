4d3947e6c6a4f7b1c26b98f091ef0bdac13e65c1
t1ww <95153968+t1ww@users.noreply.github.com>
2024-08-29 21:34:03 +0700

    5.2

---
+++ src/main/java/se331/lab/rest/dao/EventDaoImpl.java
@@ -0,0 +1,141 @@
+package se331.lab.rest.dao;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+import se331.lab.rest.entity.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class EventDaoImpl implements EventDao {
+    List<Event> eventList;
+
+    @PostConstruct
+    public void init() {
+        eventList = new ArrayList<>();
+
+        eventList.add(Event.builder()
+                .id(123L)
+                .category("animal welfare")
+                .title("Cat Adoption Day")
+                .description("Find your new feline friend at this event.")
+                .location("Meow Town")
+                .date("January 28, 2022")
+                .time("12:00")
+                .petAllowed(true)
+                .organizer("Kat Laydee")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(456L)
+                .category("food")
+                .title("Community Gardening")
+                .description("Join us as we tend to the community edible plants.")
+                .location("Flora City")
+                .date("March 14, 2022")
+                .time("10:00")
+                .petAllowed(true)
+                .organizer("Fern Pollin")
+                .build());
+
+        // Adding the rest of the events
+        eventList.add(Event.builder()
+                .id(5928101L)
+                .category("animal welfare")
+                .title("Cat Adoption Day")
+                .description("Find your new feline friend at this event.")
+                .location("Meow Town")
+                .date("January 28, 2022")
+                .time("12:00")
+                .petAllowed(true)
+                .organizer("Kat Laydee")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(4582797L)
+                .category("food")
+                .title("Community Gardening")
+                .description("Join us as we tend to the community edible plants.")
+                .location("Flora City")
+                .date("March 14, 2022")
+                .time("10:00")
+                .petAllowed(true)
+                .organizer("Fern Pollin")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(8419988L)
+                .category("sustainability")
+                .title("Beach Cleanup")
+                .description("Help pick up trash along the shore.")
+                .location("Playa Del Carmen")
+                .date("July 22, 2022")
+                .time("11:00")
+                .petAllowed(false)
+                .organizer("Carey Wales")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(8175648L)
+                .category("sport")
+                .title("Swimming")
+                .description("Let's be the first who can swim in the ocean.")
+                .location("Phuket")
+                .date("July 12, 2022")
+                .time("15:00")
+                .petAllowed(false)
+                .organizer("Ned")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(1002L)
+                .category("food")
+                .title("Canned Food Drive")
+                .description("Bring your canned food to donate to those in need.")
+                .location("Tin City")
+                .date("September 14, 2022")
+                .time("3:00")
+                .petAllowed(true)
+                .organizer("Kahn Opiner")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(1003L)
+                .category("sustainability")
+                .title("Highway Cleanup")
+                .description("Help pick up trash along the highway.")
+                .location("Highway 50")
+                .date("July 22, 2022")
+                .time("11:00")
+                .petAllowed(false)
+                .organizer("Brody Kill")
+                .build());
+    }
+
+    public Integer getEventSize() {
+        return eventList.size();
+    }
+
+    public List<Event> getEvents(Integer pageSize, Integer page) {
+        pageSize = pageSize == null ? eventList.size() : pageSize;
+        page = page == null ? 1 : page;
+        Integer firstIndex = (page - 1) * pageSize;
+        List<Event> output = new ArrayList<>();
+        for (int i = firstIndex; i < firstIndex + pageSize && i < eventList.size(); i++) {
+            output.add(eventList.get(i));
+        }
+        return output;
+    }
+
+    public Event getEventById(Long id) {
+        Event output = null;
+        for (Event event : eventList) {
+            if (event.getId().equals(id)) {
+                output = event;
+                break;
+            }
+        }
+        return output;
+    }
+}
---
+++ src/main/java/se331/lab/rest/service/EventService.java
@@ -0,0 +1,5 @@
+package se331.lab.rest.service;
+
+public class EventService {
+
+}
