9dea5b9270c9e061b6218f4e7cda175d8da9b958
t1ww <95153968+t1ww@users.noreply.github.com>
2024-09-12 15:41:34 +0700

    3.4 Create InitApp.java

---
+++ src/main/java/se331/lab/rest/config/InitApp.java
@@ -0,0 +1,61 @@
+package se331.lab.rest.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import se331.lab.rest.entity.Event;
+import se331.lab.rest.repository.EventRepository;
+
+@Component
+@RequiredArgsConstructor
+public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
+    final EventRepository eventRepository;
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        eventRepository.save(
+                Event.builder()
+                        .category("Academic")
+                        .title("Midterm Exam")
+                        .description("A time for taking exam")
+                        .location("CAMT building")
+                        .date("3rd Sept")
+                        .time("3.00-4.00pm.")
+                        .petsAllowed(false)
+                        .organizer("CAMT").build()
+        );
+        eventRepository.save(
+                Event.builder()
+                        .category("Academic")
+                        .title("Commencement Day")
+                        .description("A time for celebration")
+                        .location("CMU Convention hall")
+                        .date("21th Jan")
+                        .time("3.00-4.00pm.")
+                        .petsAllowed(false)
+                        .organizer("CMU").build()
+        );
+        eventRepository.save(
+                Event.builder()
+                        .category("Cultural")
+                        .title("Loy Krathong")
+                        .description("A time for Krathong")
+                        .location("Ping River")
+                        .date("21th Nov")
+                        .time("8.00-10.00pm.")
+                        .petsAllowed(false)
+                        .organizer("Chiang Mai").build()
+        );
+        eventRepository.save(
+                Event.builder()
+                        .category("Cultural")
+                        .title("Songkran")
+                        .description("Let's play water")
+                        .location("Chiang Mai Moat")
+                        .date("13th April")
+                        .time("10.00am-6.00pm.")
+                        .petsAllowed(true)
+                        .organizer("Chiang Mai Municipality").build()
+        );
+    }
+}
