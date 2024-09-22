875fd2c7749c70f5cf7bc1a23e155f5b036b3ec0
t1ww <95153968+t1ww@users.noreply.github.com>
2024-09-22 21:37:17 +0700

    2.4

--- src/main/java/se331/lab/rest/config/InitApp.java
+++ src/main/java/se331/lab/rest/config/InitApp.java
@@ -5,15 +5,23 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import se331.lab.rest.entity.Event;
+import se331.lab.rest.entity.Organizer;
 import se331.lab.rest.repository.EventRepository;
+import se331.lab.rest.repository.OrganizerRepository;
 
 @Component
 @RequiredArgsConstructor
 public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     final EventRepository eventRepository;
+    final OrganizerRepository organizerRepository;
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        eventRepository.save(
+        Organizer org1, org2, org3;
+        org1 = organizerRepository.save(Organizer.builder().name("CAMT").build());
+        org2 = organizerRepository.save(Organizer.builder().name("CMU").build());
+        org3 = organizerRepository.save(Organizer.builder().name("Chiang Mai").build());
+        Event tempEvent;
+        tempEvent = eventRepository.save(
                 Event.builder()
                         .category("Academic")
                         .title("Midterm Exam")
@@ -24,7 +32,10 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                         .petsAllowed(false)
                         .build()
         );
-        eventRepository.save(
+        tempEvent.setOrganizer(org1);
+        org1.getOwnEvents().add(tempEvent);
+        // //
+        tempEvent = eventRepository.save(
                 Event.builder()
                         .category("Academic")
                         .title("Commencement Day")
@@ -35,7 +46,10 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                         .petsAllowed(false)
                         .build()
         );
-        eventRepository.save(
+        tempEvent.setOrganizer(org1);
+        org1.getOwnEvents().add(tempEvent);
+        // //
+        tempEvent = eventRepository.save(
                 Event.builder()
                         .category("Cultural")
                         .title("Loy Krathong")
@@ -46,7 +60,10 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                         .petsAllowed(false)
                         .build()
         );
-        eventRepository.save(
+        tempEvent.setOrganizer(org2);
+        org2.getOwnEvents().add(tempEvent);
+        // //
+        tempEvent = eventRepository.save(
                 Event.builder()
                         .category("Cultural")
                         .title("Songkran")
@@ -57,5 +74,7 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
                         .petsAllowed(true)
                         .build()
         );
+        tempEvent.setOrganizer(org3);
+        org3.getOwnEvents().add(tempEvent);
     }
 }
