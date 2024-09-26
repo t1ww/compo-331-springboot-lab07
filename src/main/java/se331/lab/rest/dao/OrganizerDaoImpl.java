aafbd6e90e399a73e55fe9e66725ddf8e80646fd
t1ww <95153968+t1ww@users.noreply.github.com>
2024-09-26 23:07:20 +0700

    6.5

--- src/main/java/se331/lab/rest/dao/OrganizerDaoImpl.java
+++ src/main/java/se331/lab/rest/dao/OrganizerDaoImpl.java
@@ -1,12 +1,13 @@
-package se331.lab.dao;
+package se331.lab.rest.dao;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
-import se331.lab.rest.dao.OrganizerDao;
 import se331.lab.rest.entity.Organizer;
 import se331.lab.rest.repository.OrganizerRepository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,5 +16,10 @@ public class OrganizerDaoImpl implements OrganizerDao {
     @Override
     public Page<Organizer> getOrganizer(Pageable pageRequest) {
         return organizerRepository.findAll(pageRequest);
+    }
+
+    @Override
+    public Optional<Organizer> findById(Long id) {
+        return organizerRepository.findById(id);
     }
 }
--- src/main/java/se331/lab/rest/service/EventServiceImpl.java
+++ src/main/java/se331/lab/rest/service/EventServiceImpl.java
@@ -1,15 +1,22 @@
 package se331.lab.rest.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import se331.lab.rest.dao.EventDao;
+import se331.lab.rest.dao.OrganizerDao;
 import se331.lab.rest.entity.Event;
+import se331.lab.rest.entity.Organizer;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
     final EventDao eventDao;
+
+    final OrganizerDao organizerDao;
     @Override
     public Integer getEventSize(){
         return eventDao.getEventSize();
@@ -25,7 +32,12 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
+    @Transactional
     public Event saveEvent(Event event) {
+        Organizer organizer =
+                organizerDao.findById(event.getOrganizer().getId()).orElse(null);
+        event.setOrganizer(organizer);
+        organizer.getOwnEvents().add(event);
         return eventDao.saveEvent(event);
     }
 }
