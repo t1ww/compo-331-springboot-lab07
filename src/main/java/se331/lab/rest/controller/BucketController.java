4524159786fcc8a01677076571ae1412b4f25352
t1ww <95153968+t1ww@users.noreply.github.com>
2024-10-07 21:14:46 +0700

    BucketController

---
+++ src/main/java/se331/lab/rest/controller/BucketController.java
@@ -0,0 +1,22 @@
+package se331.lab.rest.controller;
+
+import jakarta.servlet.ServletException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import se331.lab.rest.util.CloudStorageHelper;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class BucketController {
+    final CloudStorageHelper cloudStorageHelper;
+    @PostMapping("/uploadFile")
+    public ResponseEntity<?> uploadFile(@RequestPart(value = "file") MultipartFile file) throws IOException, ServletException {
+        return ResponseEntity.ok(this.cloudStorageHelper.getImageUrl(file,"compolab11.appspot.com"));
+    }
+}
